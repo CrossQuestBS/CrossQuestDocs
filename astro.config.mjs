c3002d8888cda8009dcab89159a917ae230a0107
Lillie <75392499+iLillie@users.noreply.github.com>
2026-03-29 20:18:20 +0200

    setup deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import starlightThemeFlexoki from 'starlight-theme-flexoki'
 import expressiveCode from 'astro-expressive-code';
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://crossquestbs.github.io',
     integrations: [expressiveCode(),starlight({
         plugins: [starlightThemeFlexoki({accentColor: "purple"})],
 	
